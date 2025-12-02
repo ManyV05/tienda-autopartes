8bb42048f00c86d3cbb89c4bf3d01f5b06cb5093
Manuel Vázquez <A01254602@tec.mx>
2025-12-02 09:21:38 -0700

    Subiendo el cpp de Autoparte. Contiene los metodos getters y setters ya definidos y funcionales

---
+++ Autoparte.cpp
@@ -0,0 +1,36 @@
+//
+// Created by manyv on 30/11/2025.
+//
+
+#include "Autoparte.h"
+
+Autoparte::Autoparte()  {
+    this->nombre="";
+    this->codigo=0000;
+    this->precio=00.0;
+};
+
+Autoparte::Autoparte(const string& cNombre, int &cCodigo, double &cPrecio){
+    this->nombre=cNombre;
+    this->codigo=cCodigo;
+    this->precio=cPrecio;
+};
+
+string Autoparte::getNombre() const {
+    return this->nombre;
+};
+void Autoparte::setNombre(const string &cNombre) {
+    this->nombre=cNombre;
+};
+int Autoparte::getCodigo() const {
+    return this->codigo;
+};
+void Autoparte::setCodigo(const int &cCodigo) {
+    this->codigo=cCodigo;
+};
+double Autoparte::getPrecio() const {
+    return this->precio;
+};
+void Autoparte::setPrecio(const double &cPrecio) {
+    this->precio=cPrecio;
+};
