fab00cc34ead00a212956980d6c12716495d9bdb
capit <capitanlego100@gmail.com>
2025-12-03 14:04:22 -0700

    verificacion de los metodos

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by capit on 12/3/2025.
+//
+
+#include "Tienda.h"
+#include <iostream>
+#include <vector>
+#
+
+int main() {
+
+    Tienda autozone;
+
+    autozone.cargarCatalogo();
+
+    autozone.listarAutopartes();
+
+    autozone.mostrarCarrito();
+
+
+    return 0;
+}
